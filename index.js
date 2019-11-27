dff81ae29fb99a3a5a08af217c9f2f3cec773be0
박진용/MC운영그룹(MC기술)/Senior Engineer/삼성SDS <jinyong82.park@samsung.com>
2019-11-27 04:26:17 +0000

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello World!_v120');
+  res.send('!Hello World!_v121');
 });
 
 var server = app.listen(8081, function () {
